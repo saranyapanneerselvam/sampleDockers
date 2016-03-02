022758b639e80140fbc6fab91d7d07759dc35f8c
lakshmim123 <lakshmi@showmetric.co>
2016-03-02 17:35:41 +0530

    Client and Server Integration have completed to display list of metrics under the channel & list of google account in dropdown view..

--- controllers/metrics.js
+++ controllers/metrics.js
@@ -10,8 +10,9 @@ module.exports = function (app) {
     app.get('/api/v1/get/metrics/:channelId', getmetricsList.metrics, function (req, res) {
         var metrics = req.showMetric.metrics;
         console.log('Here it comes');
-        if (metrics)
-            res.json({metricsList: metrics});
+        if (metrics){
+        console.log(metrics);
+            res.json({metricsList: metrics});}
         else
             res.status(500).send({error: ""});
     });

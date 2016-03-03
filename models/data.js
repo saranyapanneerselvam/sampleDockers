7b777614c12835a01cb63a7a5a0a6d7fe4e83ae2
Rajalakshmi <rajalakshmi@showmetric.co>
2016-03-03 18:18:59 +0530

    committed data model

---
+++ models/data.js
@@ -0,0 +1,18 @@
+// app/models/user.js
+// load the things we need
+var mongoose = require('mongoose');
+
+
+// define the schema for our user model
+var dataSchema = mongoose.Schema({
+    objectId: String,
+    metricId: String,
+    data: Object,
+    created: Date,
+    updated: Date,
+    deleted: Date
+});
+
+// create the model for organization and expose it to our app
+module.exports = mongoose.model('Data', dataSchema);
+

4d85a0d80792b5552ba5fd4f2e672d5d8d5baa97
saranyapanneerselvam <saranya@showmetric.co>
2016-04-04 10:50:18 +0530

    integrating facebookAds

--- controllers/twitterAuth.js
+++ controllers/twitterAuth.js
@@ -1,5 +1,12 @@
 var request = require('request');
+var merticsList = require ('../middlewares/twitterMetrics');
 var configAuth = require('../config/auth');
+var profile = require('../models/profiles');
+var channels = require('../models/channels');
+var metrics = require('../models/metrics');
+var Twitter = require('twitter');
+var user = require('../helpers/user');
+var createDate = new Date().getTime();
 module.exports = function (app) {
     var OAuth = require('oauth').OAuth;
     var oa = new OAuth(
@@ -12,7 +19,7 @@ module.exports = function (app) {
         "HMAC-SHA1"
     );
 
-    app.get('/api/v1/auth/twitter', function (req, res) {
+    app.get('/api/auth/twitter', function (req, res) {
         oa.getOAuthRequestToken(function (error, oauth_token, oauth_token_secret, results) {
             if (error) {
                 console.log(error);
@@ -24,12 +31,15 @@ module.exports = function (app) {
                 console.log('oauth.token: ' + oauth_token);
                 req.session.oauth.token_secret = oauth_token_secret;
                 console.log('oauth.token_secret: ' + oauth_token_secret);
+                //If response of the storeProfiles function is success then redirect it to profile page
                 res.redirect('https://twitter.com/oauth/authenticate?oauth_token=' + oauth_token)
+
+
             }
         });
     });
     app.get('/auth/twitter/callback', function (req, res, next) {
-       // console.log('session', req.session);
+        // console.log('session', req.session);
         if (req.session.oauth) {
             req.session.oauth.verifier = req.query.oauth_verifier;
             var oauth = req.session.oauth;
@@ -43,14 +53,33 @@ module.exports = function (app) {
                         req.session.oauth.access_token = oauth_access_token;
                         req.session.oauth, access_token_secret = oauth_access_token_secret;
                         console.log(results);
-                        request('https://api.twitter.com/1.1/users/show.json?screen_name=' + results.screen_name, function (err, response, body) {
-                            console.log('response',response,'body', body);
-                        })
-                        res.send("worked. nice one.");
+                        req.tokens = oauth_access_token;
+                        req.userId = results.user_id;
+                        req.profileName =  results.screen_name;
+                        req.userEmail = results.screen_name;
+                        tokens = req.tokens;
+                        console.log('access token: '+ tokens);
+                        channels.findOne({code : configAuth.channels.twitter},function(err ,channelList){
+                            console.log(channelList);
+                            req.channelId =channelList._id ;
+                            req.channelCode = '4';
+                            user.storeProfiles(req, function (err, response ) {
+                                if (err)
+                                    res.json('Error');
+                                else {
+                                    //If response of the storeProfiles function is success then redirect it to profile page
+                                    res.redirect('/profile');
+
+
+                                }
+                            });
+                        });
+
                     }
                 }
             );
         } else
             next(new Error("you're not supposed to be here."))
     });
+
 }

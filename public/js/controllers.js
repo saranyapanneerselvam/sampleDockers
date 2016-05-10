f86974e3d94d18aa8b5744d8917b16ac8c1d7ef0
Santhosh Kumar <santhosh.vkumar@gmail.com>
2016-05-10 13:09:49 +0530

    updated for custom data line chart with multiple data for same custom metric name by sathish.

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -14,15 +14,8 @@ showMetricApp.service('createWidgets',function($http,$q){
             unformattedWidget = widget;
 
             if(unformattedWidget.widgetType=="custom"){
-                // Need to get custom data
-                // Need to get custom data
-                unformattedWidget.charts = charts;
 
-                for(i=0;i<unformattedWidget.charts.length;i++) {
-                    for(getCustomName in unformattedWidget.charts[i].chartData){
-                        // console.log(unformattedWidget.charts[i].chartData[getCustomName].name);
-                    }
-                }
+                unformattedWidget.charts = charts;
 
                 finalWidget = formatDataPoints(unformattedWidget);
                 finalWidget.then(function successCallback(finalWidget){
@@ -149,28 +142,11 @@ showMetricApp.service('createWidgets',function($http,$q){
         function formatDataPoints(widget) {
             var formattedWidget = $q.defer();
             var splitDate, newDate, inputDate;
-            var getTimeStamp = "";
             var changedWidget = widget;
             for(i=0;i<widget.charts.length;i++){
                 var formattedChartData = [];
-                console.log(widget.charts[i].chartType);
-                debugger;
-                if(widget.charts[i].chartType=="cumulativeLine"){
-                    console.log('In the else condition. Needs to be addressed as chart type is cumulativeLine');
-                    console.log(widget.charts);
-
-                    for(j=0;j<widget.charts[i].chartData.length;j++){
-
-                        getTimeStamp = new Date(widget.charts[i].chartData[j].date).getTime();
-
-                        console.log(widget.charts[i].chartData[j].name+" --- "+parseInt(widget.charts[i].chartData[j].total)+"  "+widget.charts[i].chartData[j].date+" --- "+getTimeStamp);
-
-                        formattedChartData.push({key: widget.charts[i].chartData[j].name, values: [ [ getTimeStamp, parseInt(widget.charts[i].chartData[j].total) ]] });
-                    }
-                    changedWidget.charts[i].chartData = formattedChartData;
-
-                }
-                else if(widget.charts[i].chartType == 'line' || 'bar'){
+                var valuesArr = new Array();
+                if(widget.charts[i].chartType == 'line' || 'bar'){
                     if(typeof(widget.charts[i].chartData[0].total) === 'object') {
                         var endpoint;
                         for(j=0;j<widget.charts[i].metricDetails.objectTypes.length;j++){
@@ -183,8 +159,39 @@ showMetricApp.service('createWidgets',function($http,$q){
                         }
                         changedWidget.charts[i].chartData = formattedChartData;
                     } else {
+                        var setCustomDataArr = new Array();
+
                         for(j=0;j<widget.charts[i].chartData.length;j++){
-                            formattedChartData.push({x: moment(widget.charts[i].chartData[j].date), y:widget.charts[i].chartData[j].total});
+                            if(widget.charts[i].chartData[j].name==undefined){
+                                formattedChartData.push({x: moment(widget.charts[i].chartData[j].date), y:widget.charts[i].chartData[j].total});
+                            }
+                            else{
+                                var IsAlreadyExist = 0;
+                                for(getData in formattedChartData){
+
+                                    if(formattedChartData[getData].key==widget.charts[i].chartData[j].name){
+                                        valuesArr = formattedChartData[getData].values;
+                                        var dataValues = {
+                                            'x': moment(widget.charts[i].chartData[j].date),
+                                            'y': widget.charts[i].chartData[j].total
+                                        };
+                                        valuesArr.push(dataValues);
+                                        formattedChartData[getData].values=valuesArr;
+                                        IsAlreadyExist = 1;
+                                    }
+                                }
+
+                                if (IsAlreadyExist != 1) {
+                                    valuesArr = [];
+                                    var dataValues = {
+                                        'x': moment(widget.charts[i].chartData[j].date),
+                                        'y': widget.charts[i].chartData[j].total
+                                    };
+                                    valuesArr.push(dataValues);
+                                    formattedChartData.push({values:valuesArr,key: widget.charts[i].chartData[j].name, color:null});
+                                }
+
+                            }
                         }
                         changedWidget.charts[i].chartData = formattedChartData;
                     }
@@ -205,38 +212,44 @@ showMetricApp.service('createWidgets',function($http,$q){
         var graphData = [];
         graphData.lineData = []; graphData.barData = [];
         graphData.lineDataOptions = []; graphData.barDataOptions = [];
-        graphData.cumulativeData = [];
-        graphData.cumulativeDataOptions = [];
+
 
         for(var i=0;i<widgetData.charts.length;i++){
-            var keyValue;
-            if(widgetData.charts[i].metricDetails!=undefined){
-                keyValue = widgetData.charts[i].metricDetails.name;
-            }
-            else{
-                keyValue = "CustomName";
-            }
             if(widgetData.charts[i].chartType == 'line'){
-                graphData.lineData.push({
-                    values: widgetData.charts[i].chartData,      //values - represents the array of {x,y} data points
-                    key: keyValue, //key  - the name of the series.
-                    color: '#7E57C2'  //color - optional: choose your own line color.
-                });
+                if(widgetData.charts[i].metricDetails!=undefined){
+                    graphData.lineData.push({
+                        values: widgetData.charts[i].chartData,      //values - represents the array of {x,y} data points
+                        key: widgetData.charts[i].metricDetails.name, //key  - the name of the series.
+                        color: '#7E57C2'  //color - optional: choose your own line color.
+                    });
+                }
+                else{
+                    for(customData in widgetData.charts[i].chartData){
+                        graphData.lineData.push({
+                            values: widgetData.charts[i].chartData[customData].values,      //values - represents the array of {x,y} data points
+                            key: widgetData.charts[i].chartData[customData].key, //key  - the name of the series.
+                            color: widgetData.charts[i].chartData[customData].color  //color - optional: choose your own line color.
+                        });
+                    }
+                }
+
             } else if (widgetData.charts[i].chartType == 'bar'){
-                graphData.barData.push({
-                    values: widgetData.charts[i].chartData,      //values - represents the array of {x,y} data points
-                    key: keyValue, //key  - the name of the series.
-                    color: '#7E57C2'  //color - optional: choose your own line color.
-                });
-            }
-
-            else if (widgetData.charts[i].chartType == 'cumulativeLine'){
-                console.log(widgetData.charts[i].chartData);
-                graphData.cumulativeData.push({
-                    values: widgetData.charts[i].chartData,      //values - represents the array of {x,y} data points
-                    key: keyValue, //key  - the name of the series.
-
-                });
+                if(widgetData.charts[i].metricDetails!=undefined){
+                    graphData.barData.push({
+                        values: widgetData.charts[i].chartData,      //values - represents the array of {x,y} data points
+                        key: widgetData.charts[i].metricDetails.name, //key  - the name of the series.
+                        color: '#7E57C2'  //color - optional: choose your own line color.
+                    });
+                }
+                else{
+                    for(customData in widgetData.charts[i].chartData){
+                        graphData.barData.push({
+                            values: widgetData.charts[i].chartData[customData].values,      //values - represents the array of {x,y} data points
+                            key: widgetData.charts[i].chartData[customData].key, //key  - the name of the series.
+                            color: widgetData.charts[i].chartData[customData].color  //color - optional: choose your own line color.
+                        });
+                    }
+                }
             }
         }
 
@@ -249,17 +262,18 @@ showMetricApp.service('createWidgets',function($http,$q){
                     y: function(d){ return d.y; },
                     useInteractiveGuideline: true,
                     xAxis: {
-                        axisLabel: 'Line Chart xaxis',
+                        axisLabel: 'Date',
                         tickFormat: function(d) {
                             return d3.time.format('%m/%d/%y')(new Date(d))}
                     },
                     yAxis: {
-                        axisLabel: 'Line Chart yaxis'
+                        axisLabel: 'Value'
                     },
                     axisLabelDistance: -10
                 }
             }
         }
+
         if(graphData.barData != null){
             graphData.barDataOptions = {
                 chart: {
@@ -280,45 +294,7 @@ showMetricApp.service('createWidgets',function($http,$q){
                 }
             }
         }
-        if(graphData.cumulativeData != null){
-            graphData.cumulativeDataOptions = {
-                chart: {
-                    type: 'cumulativeLineChart',
-                    height: 450,
-                    margin : {
-                        top: 20,
-                        right: 20,
-                        bottom: 60,
-                        left: 65
-                    },
-                    x: function(d){ return d[0]; },
-                    y: function(d){ return d[1]/100; },
-                    average: function(d) { return d.mean/100; },
 
-                    color: d3.scale.category10().range(),
-                    duration: 300,
-                    useInteractiveGuideline: true,
-                    clipVoronoi: false,
-
-                    xAxis: {
-                        axisLabel: 'X Axis',
-                        tickFormat: function(d) {
-                            return d3.time.format('%m/%d/%y')(new Date(d))
-                        },
-                        showMaxMin: false,
-                        staggerLabels: true
-                    },
-
-                    yAxis: {
-                        axisLabel: 'Y Axis',
-                        tickFormat: function(d){
-                            return d3.format(',.1%')(d);
-                        },
-                        axisLabelDistance: 20
-                    }
-                }
-            }
-        }
 
         var tempChart = {
             'options': graphData.lineDataOptions,

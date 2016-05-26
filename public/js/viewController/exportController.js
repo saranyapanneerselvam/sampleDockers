2087283bf3118b12626289f8edbfc1bf735f4e68
Santhosh Kumar <santhosh.vkumar@gmail.com>
2016-05-26 10:36:20 +0530

    updaed alert ui for export from copy clipboard function call.

--- public/js/viewController/exportController.js
+++ public/js/viewController/exportController.js
@@ -7,33 +7,19 @@ function ExportController($scope,$http,$state,$rootScope,$window,$stateParams,ge
         var setPDFOption = $("#exportOptionPDF").prop("checked");
         var dashboardLayout = document.getElementById('dashboardLayout');
         if(setJPEGOption==true){
-            $rootScope.fileType = "JPEG";
+            swal("Please wait while the JPEG file is being downloading.", "", "success");
             console.log("to export JPEG");
             domtoimage.toBlob(document.getElementById('dashboardLayout'))
                 .then(function (blob) {
                     window.saveAs(blob, 'dashboardWidgets.jpeg');
-
                 });
         }
 
         if(setPDFOption==true){
-            $rootScope.fileType = "PDF";
+            swal("Please wait while the PDF file is being downloading.", "", "success");
             console.log("to export PDF");
-
-            if(setPDFOption==true){
-                $rootScope.fileType = "PDF";
-                console.log("to export PDF");
-
                 domtoimage.toPng(dashboardLayout)
                     .then(function (dataUrl) {
-                        // var img = new Image();
-                        // img.src = dataUrl;
-                        // var doc = new jsPDF({
-                        //     format:'a2'
-                        // });
-                        // doc.addImage(img, 'JPEG', 20, 20);
-                        // doc.save('dashboardWidgets.pdf');
-
                         var jsonData = {
                             "dashboardLayout": dataUrl
                         };
@@ -42,6 +28,7 @@ function ExportController($scope,$http,$state,$rootScope,$window,$stateParams,ge
                             method: 'POST', url: '/api/v1/createHtml5ToPdf/dashboard', data: jsonData
                         }).then(function successCallback(response){
                             console.log(response);
+
                         }, function errorCallback (error){
                             console.log('Error in creating PDF dashboard widgets',error);
 
@@ -52,7 +39,6 @@ function ExportController($scope,$http,$state,$rootScope,$window,$stateParams,ge
                     console.error('oops, something went wrong!', error);
                 });
 
-            }
 
         }
     };

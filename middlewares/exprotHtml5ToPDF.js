d07c396bf1b159f058e9b40549d34926d61b0fdc
santhosh Windows Desktop <santhosh.vkumar@gmail.com>
2016-06-18 12:53:15 +0530

    fixed for PDF generation bug fix by sathish for both windows and mac os by Sathish.

--- middlewares/exprotHtml5ToPDF.js
+++ middlewares/exprotHtml5ToPDF.js
@@ -1,9 +1,11 @@
-var html5pdf = require("html5-to-pdf");
+var pdf = require('html-pdf');
+var fs = require('fs');
+var options = { format:"A3", quality:"75", orientation:"landscape" };
 var exports = module.exports = {};
 
 exports.createHtml5ToPdf = function (req, res, next) {
     //console.log("Html : "+req.body.dashboardLayout);
-    var html = "<img src='"+req.body.dashboardLayout+"' />";
+    var html = "<img src='"+req.body.dashboardLayout+"' style='width: 100%;' />";
     var timestamp = Number(new Date());
     var outputPath = "public/PDF/"+req.body.dashboardName+"_"+timestamp+".pdf";
 
@@ -12,9 +14,17 @@ exports.createHtml5ToPdf = function (req, res, next) {
         next();
     }
     else{
-        html5pdf({paperFormat:'Legal'}).from.string(html).to(outputPath, function () {
+       /* htmlTopdf({paperFormat:'Legal'}).from.string(html).to(outputPath, function () {
             req.app.result = {'status': '200','Response': "/PDF/"+req.body.dashboardName+"_"+timestamp+".pdf"};
             next();
-        })
+        })*/
+
+        pdf.create(html,options).toStream(function(err, stream){
+            stream.pipe(fs.createWriteStream(outputPath));
+            req.app.result = {'status': '200','Response': outputPath};
+            next();
+        });
+
+
     }
 };

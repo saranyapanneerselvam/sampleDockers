f67a76f7f055391e07370622a9f5239398e9a21a
Santhosh iMac <santhosh.vkumar@gmail.com>
2016-06-20 14:51:15 +0530

    updated for download PDF bug fix for both windows and mac by sathish.

--- middlewares/exprotHtml5ToPDF.js
+++ middlewares/exprotHtml5ToPDF.js
@@ -7,7 +7,16 @@ exports.createHtml5ToPdf = function (req, res, next) {
     //console.log("Html : "+req.body.dashboardLayout);
     var html = "<img src='"+req.body.dashboardLayout+"' style='width: 100%;' />";
     var timestamp = Number(new Date());
-    var outputPath = "public/PDF/"+req.body.dashboardName+"_"+timestamp+".pdf";
+    var getName = "";
+
+    if(req.body.dashboardName==undefined || req.body.dashboardName=="undefined"){
+        getName = "No_Name";
+    }
+    else{
+        getName = req.body.dashboardName;
+    }
+
+    var outputPath = "public/PDF/"+getName+"_"+timestamp+".pdf";
 
     if(html==undefined || html==null){
         req.app.result = {'status': '302','Response': 'Error in creating PDF'};
@@ -21,7 +30,7 @@ exports.createHtml5ToPdf = function (req, res, next) {
 
         pdf.create(html,options).toStream(function(err, stream){
             stream.pipe(fs.createWriteStream(outputPath));
-            req.app.result = {'status': '200','Response': outputPath};
+            req.app.result = {'status': '200','Response': '/PDF/'+getName+'_'+timestamp+'.pdf'};
             next();
         });
 

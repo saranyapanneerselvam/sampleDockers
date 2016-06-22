125945c1b01284b142f7d1a3056da3ea8a0045d0
NutChan <natarajan@showmetric.co>
2016-06-22 11:41:36 +0530

    Changes for Live Server

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
     //'url': 'mongodb://showmetric:showmetric@ds013918.mlab.com:13918/showmetric' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    'url': 'mongodb://datapoolt:datapoolt@localhost:27017/datapoolt_14052016' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url': 'mongodb://datapoolt:datapoolt@localhost:27017/Datapoolt' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 };

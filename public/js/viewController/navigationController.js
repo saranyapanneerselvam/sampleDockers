2e04f303436105bd09f5052d88b8f3664db237f9
Rajeshkumar V <Rajeshkumar V>
2016-07-25 15:02:05 +0530

    CSS changes

--- public/js/viewController/navigationController.js
+++ public/js/viewController/navigationController.js
@@ -9,37 +9,6 @@ function NavigationController() {
         $("#insightsModalContent").addClass('md-show');
     });
 
-
-    $(".settingModalContent").on( 'click', function( ev ) {
-        $(".navbar").css('z-index','1');
-        $(".md-overlay").css("background","rgba(0,0,0,0.5)");
-        $("#settingModalContent").addClass('md-show');
-    });
-
-
-    $(".addRemoveModalContent").on( 'click', function( ev ) {
-        console.log('CALLED')
-        $(".navbar").css('z-index','1');
-        $(".md-overlay").css("background","rgba(0,0,0,0.5)");
-        $("#addRemoveModalContent").addClass('md-show');
-    });
-
-    $(".modifyUserModalContent").on( 'click', function( ev ) {
-        console.log('CALLED')
-        $(".navbar").css('z-index','1');
-        $(".md-overlay").css("background","rgba(0,0,0,0.5)");
-        $("#modifyUserModalContent").addClass('md-show');
-    });
-
-
-    $(".newAccountModalContent").on( 'click', function( ev ) {
-        console.log('CALLED')
-        $(".navbar").css('z-index','1');
-        $(".md-overlay").css("background","rgba(0,0,0,0.5)");
-        $("#newAccountModalContent").addClass('md-show');
-    });
-
-    
     function removeModal( hasPerspective ) {
         classie.remove( modal, 'md-show' );
 
---
+++ public/js/viewController/topNavbarController.js
@@ -0,0 +1,37 @@
+showMetricApp.controller('TopNavbarController',TopNavbarController)
+
+function TopNavbarController() {
+
+
+    $(".addRemoveModalContent").on( 'click', function( ev ) {
+        $(".navbar").css('z-index','1');
+        $(".md-overlay").css("background","rgba(0,0,0,0.5)");
+        $("#addRemoveModalContent").addClass('md-show');
+    });
+
+    $(".modifyUserModalContent").on( 'click', function( ev ) {
+        $(".navbar").css('z-index','1');
+        $(".md-overlay").css("background","rgba(0,0,0,0.5)");
+        $("#modifyUserModalContent").addClass('md-show');
+    });
+
+    $(".newAccountModalContent").on( 'click', function( ev ) {
+        $(".navbar").css('z-index','1');
+        $(".md-overlay").css("background","rgba(0,0,0,0.5)");
+        $("#newAccountModalContent").addClass('md-show');
+    });
+
+
+    function removeModal( hasPerspective ) {
+        classie.remove( modal, 'md-show' );
+
+        if( hasPerspective ) {
+            classie.remove( document.documentElement, 'md-perspective' );
+        }
+    }
+
+    function removeModalHandler() {
+        removeModal( classie.has( el, 'md-setperspective' ) );
+    }
+
+}

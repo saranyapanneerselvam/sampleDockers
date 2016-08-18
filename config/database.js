7d254497a6046f1cfb7f87376f4f37e00f52bbf9
Rajeshkumar V <Rajeshkumar V>
2016-08-18 21:10:17 +0530

    Merge branch 'developmentLatestVersion' of C:\Users\Rajeshkumar\WebstormProjects\ShowmetricBetaMaster with conflicts.

--- config/database.js
+++ config/database.js
@@ -5,5 +5,5 @@ module.exports = {
     //'url': 'mongodb://datapoolt:DAtaPOolT@ec2-52-76-6-41.ap-southeast-1.compute.amazonaws.com/Datapoolt'
     //'url': 'mongodb://admin:admin@localhost:27017/datapoolt_CustomFusions' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
     //'url': 'mongodb://admin:admin@ds015334.mlab.com:15334/datapoolt15062016'
-    'url': 'mongodb://saranya:saranya@ds031975.mlab.com:31975/datapooltlocal'
+    'url': 'mongodb://admin:admin@localhost:27017/datapoolt_10082016'
 };
